61faa45c46e4ab9e091ccb54a3fdf1c842c17360
UkiHong <suhong9507@gmail.com>
2024-01-18 00:57:16 +0000

    finished creating apps

---
+++ direct_messages/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from common.models import CommonModel
+
+
+class ChattingRoom(CommonModel):
+    # Room (Chat room) model definition
+
+    users = models.ManyToManyField(
+        "users.User",
+    )
+
+    def __str__(self) -> str:
+        return "Chatting Room."
+
+
+class Message(CommonModel):
+    # Message Model definition
+    text = models.TextField()
+    user = models.ForeignKey(
+        "users.User",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    room = models.ForeignKey(
+        "direct_messages.ChattingRoom",
+        on_delete=models.CASCADE,
+        # direct_messages app의 (chat)Room 이 삭제되면 messages 도 삭제
+    )
+
+    def __str__(self) -> str:
+        return f"{self.user} says: {self.text}"

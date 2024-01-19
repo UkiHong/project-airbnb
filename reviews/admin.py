857cb24f078528123450dd61c1ed83c950c528b1
UkiHong <suhong9507@gmail.com>
2024-01-19 02:16:19 +0000

    power admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,5 +1,45 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
 from .models import Review
+
+
+# 8.3 Custom Filters
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            reviews
+
+
+class ScoreFilter(admin.SimpleListFilter):
+    title = "Filter by Score"
+    parameter_name = "score"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("bad", "Bad"),
+            ("good", "Good"),
+        ]
+
+    def queryset(self, request, reviews):
+        score = self.value()
+        if score:
+            return reviews.filter(rating__lt=3)
+        else:
+            return reviews.filter(rating__gte=3)
 
 
 @admin.register(Review)
@@ -9,4 +49,12 @@ class ReviewAdmin(admin.ModelAdmin):
         "payload",
     )
 
-    list_filter = ("rating",)
+    list_filter = (
+        WordFilter,
+        ScoreFilter,
+        "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
+        # ForeignKey 를 가진 value에 대해 필터링 옵션을 만들 수 있음
+    )
--- rooms/admin.py
+++ rooms/admin.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(description="Set all prices to zero")
+def reset_prices(model_admin, request, rooms):
+    # 3개의 parameter 가 꼭 필요함
+    # 1st = 이 액션을 호출한 model_admin, 2st = 이 액션을 호출한 유저 정보를 가진 request 객체
+    # 3rd = queryset (여기서 rooms 로 이름 붙임)
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    actions = (reset_prices,)
     list_display = (
         "name",
         "price",
@@ -25,6 +36,15 @@ class RoomAdmin(admin.ModelAdmin):
 
     def total_amenities(self, room):
         return room.amenities.count()
+
+    search_fields = (
+        "name",
+        "^price",
+        "owner__username",
+        # __username 은 owner 의 username 을 검색
+        # ^ 는 start with --- 로 시작하는 값만 검색
+        # = 는 동일한 값만 검색, 아무것도 안 붙이면 contains 로 검색
+    )
 
 
 @admin.register(Amenity)

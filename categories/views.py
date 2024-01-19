878e6681f73b7197a14adce3e9e3b04d5fdbc3d0
UkiHong <suhong9507@gmail.com>
2024-01-19 17:15:26 +0000

    api without restframework -> not gonna use it again

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+####
+### django restframework 를 사용하지않고 만든 API
+### objects 를 불러올 때마다 새로 serializer 를 해줘야하기 때문에 비효율적
+from django.http import JsonResponse
+from django.core import serializers
+from .models import Category
 
-# Create your views here.
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return JsonResponse(
+        {
+            "ok": True,
+            "categories": serializers.serialize("json", all_categories),
+        }
+    )

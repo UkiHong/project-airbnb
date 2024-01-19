66a72d4dc6e2f99fd14aad821334e9783b34fe1e
UkiHong <suhong9507@gmail.com>
2024-01-19 17:27:06 +0000

    category serializer with a manual method but not gonna use it bcs it's repetitive

--- categories/views.py
+++ categories/views.py
@@ -1,16 +1,16 @@
-####
-### django restframework 를 사용하지않고 만든 API
-### objects 를 불러올 때마다 새로 serializer 를 해줘야하기 때문에 비효율적
-from django.http import JsonResponse
-from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
+from .serializers import CategorySerializer
 
 
+@api_view()
 def categories(request):
     all_categories = Category.objects.all()
-    return JsonResponse(
+    serializer = CategorySerializer(all_categories, many=True)
+    return Response(
         {
             "ok": True,
-            "categories": serializers.serialize("json", all_categories),
+            "categories": serializer.data,
         }
     )

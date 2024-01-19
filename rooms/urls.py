91884764bb923afe0f27539025dfe8f79ae9366b
UkiHong <suhong9507@gmail.com>
2024-01-19 04:58:18 +0000

    django template complete but will be replaced

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.see_all_rooms),
-    path("<int:room_id>", views.see_one_room),
+    path("<int:room_pk>", views.see_one_room),
     # "<int:room_id>"는 rooms/<??> 의 parameter 를 의미, int 는 숫자를 의미
 ]

ad1c648a921d59bb70c25c401c40547f74196855
UkiHong <suhong9507@gmail.com>
2024-01-20 04:46:29 +0000

    rooms/amenities, room/amenities/pk views, serializers

---
+++ rooms/views.py
@@ -0,0 +1,55 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework.status import HTTP_204_NO_CONTENT
+from .models import Amenity
+from .serializers import AmenitySerializer
+
+
+class Amenities(APIView):
+    def get(self, request):
+        # 모든 View function은 자동으로 request를 받음
+        all_amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(all_amenities, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AmenitySerializer(data=request.data)
+        if serializer.is_valid():
+            amenity = serializer.save()
+            return Response(AmenitySerializer(amenity).data)
+        else:
+            return Response(serializer.errors)
+
+
+class AmenityDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Amenity.objects.get(pk=pk)
+        except Amenity.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        amenity = self.get_object(pk)
+        serializer = AmenitySerializer(amenity)
+        return Response(serializer.data)
+
+    def post(self, request, pk):
+        amenity = self.get_object(pk)
+        serializer = AmenitySerializer(
+            amenity,
+            data=request.data,
+            partial=True,
+            # partial 을 사용하면 데이터 전체가 아닌 일부만 바꿀 수 있음.
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            # 여기선 def create 가 아닌 def update 를 실행
+            return Response(AmenitySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        amenity = self.get_object(pk)
+        amenity.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
